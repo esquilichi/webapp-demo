24b8635b4ed82dd4f5b7eff5a0dafa8a8c7fbb88
Ismael Esquilichi <67276772+esquilichi@users.noreply.github.com>
2022-02-28 00:44:32 +0100

    Commit prueba workflow

--- src/main/java/com/urjc/es/webapp/controllers/WebController.java
+++ src/main/java/com/urjc/es/webapp/controllers/WebController.java
@@ -11,7 +11,7 @@ public class WebController {
     @RequestMapping("/")
     public ModelAndView helloworld(){
         ModelAndView mvc = new ModelAndView("index");
-        mvc.addObject("name", "World!");
+        mvc.addObject("name", "Mundo!");
         return mvc;
     }
 
