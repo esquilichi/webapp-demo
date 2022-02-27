ffd3ad1b0f36100183be136a75f412a5f6a1497c
Ismael Esquilichi <67276772+esquilichi@users.noreply.github.com>
2022-02-28 00:29:12 +0100

    first commit

---
+++ src/main/java/com/urjc/es/webapp/controllers/WebController.java
@@ -0,0 +1,18 @@
+package com.urjc.es.webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/")
+    public ModelAndView helloworld(){
+        ModelAndView mvc = new ModelAndView("index");
+        mvc.addObject("name", "World!");
+        return mvc;
+    }
+
+}
